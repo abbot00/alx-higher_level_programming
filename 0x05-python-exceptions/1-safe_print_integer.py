07ed2523dca20a397c738c79fdb02bdf6af68ebf
abbot00 <abbot_jude@outlook.com>
2022-07-18 16:20:37 +0000

    correction

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,8 @@
 def safe_print_integer(value):
     try:
         print('{:d}'.format(value))
-        except Exception as error:
+    except Exception as error:
             return False
-        else:
+    else:
             return True
             
