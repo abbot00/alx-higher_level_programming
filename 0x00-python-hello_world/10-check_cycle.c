52be3da282835530d95c2bcba3800516012b54d5
abbot00 <abbot_jude@outlook.com>
2023-08-05 14:32:55 +0100

    update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -38,21 +38,24 @@ return (1);
 return (0);
 }
 */
+
 int check_cycle(listint_t *list) {
+    listint_t *slow, *fast;
+
     if (list == NULL || list->next == NULL) {
         return 0;
     }
 
-    listint_t *slow = list;
-    listint_t *fast = list->next;
+    slow = list;
+    fast = list->next;
 
     while (fast != NULL && fast->next != NULL) {
         if (slow == fast) {
-            return 1; // Cycle detected
+            return 1; /* Cycle detected */
         }
         slow = slow->next;
         fast = fast->next->next;
     }
 
-    return 0; // No cycle found
+    return 0; /* No cycle found */
 }
