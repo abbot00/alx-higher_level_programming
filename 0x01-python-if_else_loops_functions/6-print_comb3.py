a56436558150fc40dc1d5bcf1bebfdf00bd909c5
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-08-06 22:59:44 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for number in range(100):
     if int(number / 10) != number % 10 and int(number / 10) < number % 10:
         print("{}{}".format(int(number / 10), number % 10), end="")
        if (number != 89):
-         print(", ", end="")
+        print(", ", end="")
 print("")
                                             
