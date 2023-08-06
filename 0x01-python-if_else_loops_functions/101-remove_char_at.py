fc86c192cb1e8e2d11024cfa3a3adf7ebc3ee362
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-08-07 00:03:56 +0100

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,9 +2,8 @@
 def remove_char_at(str, n):
     if n < 0 or n >= len(str):
         return str
-    i = 0
     new_str = ""
     for ch in range(len(str)):
-        if i != n:
-            new_str += ch
+        if ch != n:
+            new_str += str[ch]
     return new_str
