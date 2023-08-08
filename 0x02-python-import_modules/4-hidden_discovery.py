b12f03cb587dc5ffa45c53c9e190de71737bfd16
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-08-08 11:27:46 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     names = dir(sorted(hidden_4))
     for name in range(names):
-        if not name.startswitch('_')
+        if not name.startswith('_')
             print(names)
