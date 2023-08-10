853520285448fcbf6f999455080bae30b0c42326
abbot00 <abbot_jude@outlook.com>
2023-08-10 05:56:34 +0100

    updated hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    l_name = names.split()
-    for word in l_name:
+    s_name = names.sort()
+    for word in s_name:
         if word[0] != '_' and word[1] != '_':
             print(word)
