d2767cb46b2822b0115d0048d571fc43ff08ec12
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-08-11 17:18:35 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""function that replaces an element in a 
-list at a specific position without 
+"""function that replaces an element in a
+list at a specific position without
 modifying the original list (like in C).
-Prototype: def new_in_list(my_list, idx, 
+Prototype: def new_in_list(my_list, idx,
 element):
 If idx is negative, the function
  should return a copy of the original list
-If idx is out of range (> of number 
+If idx is out of range (> of number
 of element in my_list), the function should
  return a copy of the original list
 You are not allowed to import any module
 You are not allowed to use try/except"""
 
+
 def new_in_list(my_list, idx, element):
-    if idx < 0 idx:
+    if idx < 0:
         return my_list
     elif idx > (len(my_list) - 1):
         return my_list
