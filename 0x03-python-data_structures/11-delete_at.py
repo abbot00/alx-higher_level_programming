d8547820b497f95005a43e0c9644327889b06b69
abbot00 <abbot_jude@outlook.com>
2023-08-12 11:30:14 +0100

    update

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#this code illustrates the 
-#function that deletes the 
-#item at a specific position in a list.
+"""this code illustrates the
+function that deletes the
+#item at a specific position in a list."""
 def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list[:]
@@ -9,7 +9,7 @@ def delete_at(my_list=[], idx=0):
         return my_list[:]
     else:
         new_list = my_list[:]
-        #del new_list[idx]
-        #return new_list
+#del new_list[idx]
+#return new_list
         del my_list[idx]
         return my_list
