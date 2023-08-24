e8d518fd27a2a8528c1511cf187bc03082f46b08
abbot00 <abbot_jude@outlook.com>
2023-08-24 16:09:44 +0100

    singly linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        self.__data = None
+        self.__next_node = None
+        self.data = data
+        self.next_node = next_node
+    
+    @property
+    def data(self):
+        return self.__data
+    
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+    
+    @property
+    def next_node(self):
+        return self.__next_node
+    
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object or None")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        
+        if self.head is None or value <= self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node is not None and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+    
+    def __str__(self):
+        result = ""
+        current = self.head
+        while current:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        return result.strip()
+
+# Example usage
+if __name__ == "__main__":
+    sll = SinglyLinkedList()
+    sll.sorted_insert(2)
+    sll.sorted_insert(5)
+    sll.sorted_insert(3)
+    sll.sorted_insert(10)
+    sll.sorted_insert(1)
+    sll.sorted_insert(-4)
+    sll.sorted_insert(-3)
+    sll.sorted_insert(4)
+    sll.sorted_insert(5)
+    sll.sorted_insert(12)
+    sll.sorted_insert(3)
+    print(sll)
