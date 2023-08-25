fbb543b2828695a872aedb77d732827570cd8374
abbot00 <abbot_jude@outlook.com>
2023-08-25 10:00:17 +0100

    task'

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" a class square that defines a square by size"""
+
+
+class Square:
+    """initialazation of the attributes of the class"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size**2
+
+    def area(self):
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
