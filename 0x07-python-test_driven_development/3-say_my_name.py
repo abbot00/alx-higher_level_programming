7a62ae680e840fcae43c31634b023fb74627a1c8
abbot00 <abbot_jude@outlook.com>
2023-08-28 08:37:19 +0100

    task 02

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,6 +8,7 @@ last_name must be a string  """
 
 
 def say_my_name(first_name, last_name=""):
+    """ function that prints the name of the user"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
