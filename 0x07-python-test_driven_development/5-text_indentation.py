c3e69e5ac51f433a948688efab321f1e744aa834
abbot00 <abbot_jude@outlook.com>
2023-08-31 08:27:31 +0100

    task

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""function that prints lines"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after '.', '?', and ':' characters.
+
+    Args:
+        text (str): The text to be processed.
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If text is not a string.
+
+    Example:
+        >>> text = "Hello. How are you? I'm good: Thanks!"
+        >>> text_indentation(text)
+        Hello.
+        How are you?
+        I'm good:
+        Thanks!
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    # Initialize variables
+    special_chars = ['.', '?', ':']
+    lines = []
+    current_line = ''
+
+    # Process each character
+    for char in text:
+        current_line += char
+        if char in special_chars:
+            lines.append(current_line.strip())
+            current_line = ''
+
+    # Print formatted lines
+    for line in lines:
+        print(line)
