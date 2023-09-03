e5694716bab7725e97b5aa0baf2cb5a86c0526fa
abbot00 <abbot_jude@outlook.com>
2023-09-03 12:54:49 +0100

    task update

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -68,3 +68,13 @@ class Rectangle:
         """ prints to stdout when a rectangle is deleted"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an intance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
